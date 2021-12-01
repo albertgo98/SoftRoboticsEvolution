42345e75f72961f703535ab4fa74c83c935b1406
albertgo98 <albertgo98@gmail.com>
2021-12-01 16:43:36 -0500

    Initial Commit

---
+++ EA_Robot_Controller2/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  EA_Robot_Controller2
+//
+//  Created by Albert Go on 12/1/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
